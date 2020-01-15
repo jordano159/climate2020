61830b9110d41900ff06f98ff5959ce3f84bcba0
Jordan Shefner <yarden11111@gmail.com>
2020-01-15 16:36:08 +0200

    Country validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -10,4 +10,23 @@ class Country < ApplicationRecord
   enum water:       %i[high medium low], _prefix: true
   enum energy:      %i[high medium low], _prefix: true
 
+  validates :name,          presence: true
+  validates :init_civ,      numericality: { greater_than: 0 }
+  validates :civ_num,       numericality: { greater_than_or_equal_to: 0 }
+  validates :deg,           numericality: { greater_than_or_equal_to: 1.0 }
+  validates :life_level,    numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+  validates :score,         numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :year,          numericality: { only_integer: true, greater_than_or_equal_to: 2020, less_than_or_equal_to: 2100 }
+  validates :is_conquered,  inclusion: { in: [true, false] }
+  validates :resilience,    inclusion: { in: Country.resiliences.keys }
+  validates :reg_rel,       inclusion: { in: Country.reg_rels.keys }
+  validates :agriculture,   inclusion: { in: Country.agricultures.keys }
+  validates :education,     inclusion: { in: Country.educations.keys }
+  validates :security,      inclusion: { in: Country.securities.keys }
+  validates :comms,         inclusion: { in: Country.comms.keys }
+  validates :social_sec,    inclusion: { in: Country.social_secs.keys }
+  validates :health,        inclusion: { in: Country.healths.keys }
+  validates :water,         inclusion: { in: Country.waters.keys }
+  validates :energy,        inclusion: { in: Country.energies.keys }
+
 end

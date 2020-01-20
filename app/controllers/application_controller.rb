3b79949e72bf0af318546704812b9970905b4288
Jordan Shefner <yarden11111@gmail.com>
2020-01-20 19:32:30 +0200

    Add did_lose checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :did_lose
 
   def home_page
   end
 
   def game_over
+  end
+
+  def did_lose
+    if params[:country_id]
+      country = Country.find(params[:country_id])
+      if country.civ_num <= 0 || country.life_level <= 0
+        country.lose = true
+      end
+      if country.lose?
+        redirect_to game_over_path
+      end
+
+    end
   end
 
 end

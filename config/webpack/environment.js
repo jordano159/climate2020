97743d0939ad1df2297ce05ce5129adafc7f6110
Pashi <guynatan@gmail.com>
2020-01-21 16:02:12 +0200

    jquery working and ministers page

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,9 +3,8 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+		$: 'jquery/src/jquery',
+     jQuery: 'jquery/src/jquery'
   })
 )
 

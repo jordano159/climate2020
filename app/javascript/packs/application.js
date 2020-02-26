ebe1edcc0c0d89206684727fed0ad04e215603dc
guy natan <guynatan@gmail.com>
2020-02-26 19:40:59 +0200

    desktop almost done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("custom/popper")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,3 +22,6 @@ import 'bootstrap'
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="modal"]')
 });
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip()
+// })
